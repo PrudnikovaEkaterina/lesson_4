c105c04a0015cad0d8b752bd92c64805d2fa6c65
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-02-25 22:27:21 +0300

    java lesson 1

--- src/main/java/JavaLesson1.java
+++ src/main/java/JavaLesson1.java
@@ -20,14 +20,35 @@ public class JavaLesson1 {
         System.out.println("d++   = " + (d++));
         System.out.println("++d   = " + (++d));
 
-        int a = 10;
-        int b = 20;
-        System.out.println("a == b = " + (a == b) );
-        System.out.println("a != b = " + (a != b) );
-        System.out.println("a > b = " + (a > b) );
-        System.out.println("a < b = " + (a < b) );
-        System.out.println("b >= a = " + (b >= a) );
-        System.out.println("b <= a = " + (b <= a) );
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("b >= a = " + (b >= a));
+        System.out.println("b <= a = " + (b <= a));
+
+
+        boolean aB = true;
+        boolean bB = false;
+        System.out.println("aB && bB = " + (aB && bB));
+        System.out.println("aB || bB = " + (aB || bB));
+        System.out.println("!(aB && bB) = " + !(aB && bB));
+
+        byte by1 = 120;
+        byte by2 = 30;
+        byte by3 = (byte) (by1 + by2);
+        System.out.println(by3);
+
+        short sh1 = 32750;
+        short sh2 = (short) (sh1 + by1);
+        System.out.println(sh2);
+
+        double do1 = 10.33;
+        int in1 = 10;
+        double sum = do1 + in1;
+        int sum1 = (int) (do1 + in1);
+        System.out.println(sum);
+        System.out.println(sum1);
 
     }
 }
